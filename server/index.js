81fdebf1ddfd9ee6234d3d9d9783dbb15d113b2b
liu-austin <austinliu279@gmail.com>
2020-02-12 20:36:03 -0800

    adds process env port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 const cors = require('cors');
 const morgan = require('morgan');
 const getter = require('./smDbHelpers/get');
